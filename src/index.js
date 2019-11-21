aee0a82a073fb515576fec67f4ef19a5a8201691
André OBROCHTA <andreobrochta@gmail.com>
2019-11-21 12:55:24 +0100

    1er version du module validate

---
+++ src/index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/*
+  Ce module va servir à détérminer si les valeurs données en paramètre sont
+  conforme
+  titre -> doit avoir au minimum 5 caractères
+  auteur -> doit contenir nom et prénom séparé par un espace avec au minimum
+  caractères
+  contenu -> doit avoir au minimum 10 caractères
+  catégorie -> doit appartenir à un élément du module 'categories'
+*/
+
+let category_valide = ['Monde', 'Sport', 'Culture', 'Voyage', 'Politique', 'Science',
+    'Divertissement', 'Musique', 'Finance', 'Justice']
+
+exports.validate = function is_valide(title, content, author, category) {
+    //On vérifie que nos entrées sont bien des string
+    if (typeof title == 'string' && typeof content == 'string' &&
+      typeof author == 'string' && typeof category == 'string') {
+
+        //on fait les vérifications du titre et contenue
+        if (title.length < 5 || content.length < 10) {
+            return 1
+        }
+
+        //on fait les vérifications de auteur
+        let author_split = author.split(' ')
+        if (author_split.length != 2 || author_split[0].length < 2 ||
+        author_split[1].length < 2) {
+            return 2
+        }
+
+        //on fait les vérifications pour la catégorie
+        for (let i = 0; i < category_valide.length; i++) {
+            //console.log(category + " " + category_valide[i])
+            if (category.localeCompare(category_valide[i]) == 0) {
+                //console.log("123")
+                return true
+            }
+        }
+        return 4
+    }
+    return 5
+}
